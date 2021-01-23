40cbe9449ee676fe9d4a911af530ccc300f96d87
zeynep yalınız <zeynepyaliniz7@gmail.com>
2021-01-23 21:29:32 +0300

    Add project files.

---
+++ KodlamaIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KodlamaIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
